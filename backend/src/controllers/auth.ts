77e04086c5c42976ee04d88c6194cd165cc2f99f
Mathgobbo <matgobbo@hotmail.com>
2023-09-28 22:25:16 -0300

    feat: Added remaining TOTP endpoints

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -11,19 +11,6 @@ import { encode } from "hi-base32";
 import * as OTPAuth from "otpauth";
 
 /**
- * Function to generate a Random Base32 string
- * that is used as secret of the User for the TOTP
- * protocol
- *
- * @returns Base32 string
- */
-const generateRandomBase32 = (bytes = 32) => {
-  const buffer = crypto.randomBytes(bytes);
-  const base32 = encode(buffer).replace(/=/g, "").substring(0, 24);
-  return base32;
-};
-
-/**
  * Function to sign up a new User
  * Using KFD algorithmns and more
  *
@@ -57,7 +44,15 @@ export const signUp = async (req: Request, res: Response) => {
     // Adding user obj to database
     const existingUsers = getAccountData();
     const newId = Object.keys(existingUsers).length + 1;
-    const newUser = { username, phone, password: hashedPass, salt, id: newId };
+
+    const newUser = {
+      username,
+      phone,
+      password: hashedPass,
+      salt,
+      id: newId,
+      otpEnabled: false,
+    };
     existingUsers[newId] = newUser;
     saveAccountData(existingUsers);
 
@@ -82,28 +77,59 @@ export const signIn = async (req: Request, res: Response) => {
     const { username, password } = req.body;
     if (!username || !password) throw new Error("missing params");
 
-    // Getting user by phone
+    // Getting user by username
     const user = getAccountDataByKey(username, "username");
     if (!user) throw new Error("user not found");
 
     // Generating the hashed password using existing user Salt and password
     // from request body
     const hashedPass = crypto
-      .scryptSync(password, user.salt, SCRYPT_KEY_LEN)
+      .scryptSync(password, user.salt, SCRYPT_KEY_LEN, {
+        N: 2048,
+        p: 1,
+        blockSize: 8,
+      })
       .toString("hex");
 
     if (user.password != hashedPass) throw new Error("Invalid Password");
 
-    return res.send({ id: user.id });
+    return res.send({
+      id: user.id,
+      username: user.username,
+      phone: user.phone,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error: (error as Error).message });
   }
 };
 
+// =========================== TOTP FUNCTIONS ===========================
+
+/**
+ * Function to generate a Random Base32 string
+ * that is used as secret of the User for the TOTP
+ * protocol
+ *
+ * @returns Base32 string
+ */
+const generateRandomBase32 = (bytes = 32) => {
+  const buffer = crypto.randomBytes(bytes);
+
+  const base32 = encode(buffer).replace(/=/g, "").substring(0, 24);
+  return base32;
+};
+
+/**
+ * Function to generate the OTP Url and OTP Secret
+ * for the user.
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
 export const generateOtp = async (req: Request, res: Response) => {
   try {
-    // Should change to use a session key!!
     // OTP secret should be a base32 string
     const { id } = req.body;
     if (!id) throw new Error("missing params");
@@ -114,9 +140,10 @@ export const generateOtp = async (req: Request, res: Response) => {
     const secret = generateRandomBase32();
 
     const totp = new OTPAuth.TOTP({
-      issuer: "toy-whats",
-      label: "Toy-Whats",
+      issuer: "ToyWhats",
+      label: user.username,
       algorithm: "SHA1",
+      period: 30,
       digits: 6,
       secret,
     });
@@ -132,6 +159,136 @@ export const generateOtp = async (req: Request, res: Response) => {
       secret,
       otpAuthUrl,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: (error as Error).message });
+  }
+};
+
+/**
+ * Function to verify if the token sent by the User is valid!
+ * This endpoint also updates the flag "otpEnabled" from the User
+ * if the token is valid
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+export const verifyOtp = async (req: Request, res: Response) => {
+  try {
+    // Getting params
+    const { id, token } = req.body;
+    if (!id || !token) throw new Error("Missing params");
+
+    // Getting user by ID
+    const user = getAccountDataByKey(id, "id");
+    if (!user) throw new Error("User not found");
+
+    // Validating if user has OTP Secret
+    if (!user.otpSecret) throw new Error("Generate secret first!");
+
+    // Generating TOTP Object
+    const totp = new OTPAuth.TOTP({
+      issuer: "ToyWhats",
+      label: user.username,
+      algorithm: "SHA1",
+      digits: 6,
+      period: 30,
+      secret: user.otpSecret,
+    });
+
+    // Validating Token sent by the user
+    const delta = totp.validate({ token });
+
+    // If `delta` is null, means that the token is invalid
+    if (delta == null) throw new Error("Invalid Token");
+
+    // Updating user
+    user["otpEnabled"] = true;
+    updateAccountData(user);
+
+    // Returning...
+    res.send({ message: "OTP Enabled!" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: (error as Error).message });
+  }
+};
+
+/**
+ * Function to validate the token ao an user
+ * that already enabled the 2FA and also verified!
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+export const validateOtp = async (req: Request, res: Response) => {
+  try {
+    // Getting params
+    const { id, token } = req.body;
+    if (!id || !token) throw new Error("Missing params");
+
+    // Getting user by ID
+    const user = getAccountDataByKey(id, "id");
+    if (!user) throw new Error("User not found");
+
+    // Validating if user has OTP enabled
+    // (which means he already verified the token)
+    if (!user.otpEnabled)
+      throw new Error("Enable 2FA first (Using verifying endpoint)");
+
+    // Generating TOTP Object
+    const totp = new OTPAuth.TOTP({
+      issuer: "ToyWhats",
+      label: user.username,
+      algorithm: "SHA1",
+      digits: 6,
+      period: 30,
+      secret: user.otpSecret,
+    });
+
+    // Validating Token sent by the user
+    const delta = totp.validate({ token });
+
+    // If `delta` is null, means that the token is invalid
+    if (delta == null) throw new Error("Invalid Token");
+    // Returning...
+    res.send({ message: "OTP valid" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: (error as Error).message });
+  }
+};
+
+/**
+ * Function to disable User's 2FA
+ * Which means that after sucessfuly calling this function
+ * He will have to call the `verifyOtp` endpoint again!
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+export const disableOtp = async (req: Request, res: Response) => {
+  try {
+    // Getting params
+    const { id } = req.body;
+    if (!id) throw new Error("Missing params");
+
+    // Getting user by ID
+    const user = getAccountDataByKey(id, "id");
+    if (!user) throw new Error("User not found");
+
+    // Validating if user has OTP Secret
+    if (!user.otpEnabled) throw new Error("2FA Already Disabled");
+
+    // Updating user
+    user["otpEnabled"] = false;
+    updateAccountData(user);
+
+    // Returning...
+    res.send({ message: "OTP DISABLED!" });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error: (error as Error).message });
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import { getUsers } from "../controllers/user";
-import { signIn, signUp } from "../controllers/auth";
+import {
+  disableOtp,
+  generateOtp,
+  signIn,
+  signUp,
+  validateOtp,
+  verifyOtp,
+} from "../controllers/auth";
 
 const router = express.Router();
 
@@ -9,6 +16,10 @@ router.get("/users", getUsers);
 router.post("/auth/sign-up", signUp);
 router.post("/auth/sign-in", signIn);
 
+router.post("/auth/generate-otp", generateOtp);
+router.post("/auth/verify-otp", verifyOtp);
+router.post("/auth/validate-otp", validateOtp);
+router.post("/auth/disable-otp", disableOtp);
 // generata TOTP
 // Validate TOTP
 // 2 routes related to TOTP
